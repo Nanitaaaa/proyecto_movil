391ec1dadd249ab721c7a80fe0b1a8c0129474bb
BASTIAN IGNACIO BASSO AGUIRRE <bbasso@utem.cl>
2024-11-09 15:15:33 -0300

    quite la franja roja que decia 'debug'

--- movil_app/lib/main.dart
+++ movil_app/lib/main.dart
@@ -13,13 +13,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home: const LoginScreen(),
     );
+    }
   }
-}
 
 
